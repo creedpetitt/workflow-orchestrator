113292d4c6c088edfa01ce4fe7235112e20da2f2
Creed Petitt <creedpetitt@gmail.com>
2026-03-06 17:20:24 -0600

    update examples to use pg url when defining a worker

--- examples/python/main.py
+++ examples/python/main.py
@@ -6,7 +6,7 @@ def main():
     worker.register("step1", lambda payload: f"step1-result: processed {payload}")
     worker.register("step2", lambda payload: f"step2-result: finished with {payload}")
 
-    worker.start()
+    worker.start("postgresql://admin:admin@postgres:5432/wf_engine")
 
 if __name__ == "__main__":
     main()
